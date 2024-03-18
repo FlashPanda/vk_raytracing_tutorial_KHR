f5d9075c1b3d71fe4317564193f13cbe4e80378b
dongqing <dongqing.czd@xuelangyun.com>
2024-03-18 20:03:10 +0800

    fix:find files

--- ray_tracing__before_copy/main.cpp
+++ ray_tracing__before_copy/main.cpp
@@ -161,8 +161,8 @@ int main(int argc, char** argv)
 
   // Creation of the example
   //helloVk.loadModel(nvh::findFile("media/scenes/cube_multi.obj", defaultSearchPaths, true));
-  helloVk.loadModel(nvh::findFile("media/scenes/Medieval_building.obj"), defaultSearchPaths, true);
-  helloVk.loadModel(nvh::findFile("media/scenes/plane.obj"), defaultSearchPaths, true);
+  helloVk.loadModel(nvh::findFile("media/scenes/Medieval_building.obj", defaultSearchPaths, true));
+  helloVk.loadModel(nvh::findFile("media/scenes/plane.obj", defaultSearchPaths, true));
 
   helloVk.createOffscreenRender();
   helloVk.createDescriptorSetLayout();
